9a6ce6fb29ed421fcbe4644a8658d932ca0a3f0e
Atahar Uddin Pollock <86181952+Pollock12@users.noreply.github.com>
2026-02-21 12:53:22 +0600

    Time: 26 ms (25.55%), Space: 7.9 MB (61.81%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
-        int ans=0;
-        for(int i=left;i<=right;i++){
-            int j=i,cnt=0;
-            while(j){
-                if(j%2)cnt++;
-                j/=2;
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            int j = i, cnt = 0;
+            while (j) {
+                if (j % 2)
+                    cnt++;
+                j /= 2;
             }
-            bool ok=true;
-            for(int k=2;k<cnt;k++){
-                if(cnt%k==0){
-                    ok=false;
-                    break;
-                }
-            }
-            if(ok && cnt>1)ans++;
+            if (cnt == 2 || cnt == 3 || cnt == 5 || cnt == 7 || cnt == 11 ||
+                cnt == 13 || cnt == 17 || cnt == 19)
+                ans++;
         }
         return ans;
     }
