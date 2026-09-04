9c849ae3e4333991b6975a4e3d80493df5c504dc
Atahar Uddin Pollock <86181952+Pollock12@users.noreply.github.com>
2026-09-05 00:01:14 +0600

    Time: 2 ms (29.25%), Space: 30.3 MB (76.6%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size(), mx = 0;
+        for (int i = 0; i < n; i++) {
+            mx = max(mx, nums[i]);
+            int mn = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                mn = min(mn, nums[j]);
+            }
+            if (mx - mn <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
