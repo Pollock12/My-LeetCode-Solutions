702b1b0a21fa12f23f5d0ed9bf65667c74d2fb4f
Atahar Uddin Pollock <86181952+Pollock12@users.noreply.github.com>
2026-09-14 00:21:43 +0600

    Time: 63 ms (41.93%), Space: 12.7 MB (98.21%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int max_ans = 0;
+        for (int down = -(n - 1); down <= (n - 1); down++) {
+            for (int right = -(n - 1); right <= (n - 1); right++) {
+                int count = 0;
+                for (int row = 0; row < n; row++) {
+                    for (int col = 0; col < n; col++) {
+                        if (img1[row][col] == 1) {
+                            int newrow = row + down;
+                            int newcol = col + right;
+                            if (newrow >= 0 && newrow < n && newcol >= 0 &&
+                                newcol < n && img2[newrow][newcol] == 1) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+                max_ans = max(max_ans, count);
+            }
+        }
+        return max_ans;
+    }
+};
